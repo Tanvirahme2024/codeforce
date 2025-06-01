0827879629b6bd874280debaddf616441d6870b1
Tanvirahme2024 <ta7142047@gmail.com>
2025-06-01 21:44:08 +0600

    A_Sauare_Year.cpp

--- A_Square_Year.cpp
+++ A_Square_Year.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-void year()
-{
-    int n;
-    cin>>n;
-    int sqr=ciel(sqrt(n));
-    if(sqr*sqr==n)
-    {
-        cout<<0<<' '<<sqr<<endl;
-    }
-    else
-    cout<<-1<<endl;
-}
-
 int main()
 {
     int t;
     cin>>t;
     while(t--)
-    year();
+    {
+        int year;
+        cin>>year;
+
+        int n=sqrt(year);
+        if(n*n==year)
+        {
+            cout<<0<<' '<<n<<endl;
+        }
+        else
+        {
+            cout<<-1<<endl;
+        }
+    }
 
 }
